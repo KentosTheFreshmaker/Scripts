c0df8e846f9b8275b2e5aea27847158d77347d0d
KentosTheFreshmaker <thekentos123@gmail.com>
2016-10-05 00:05:17 -0500

    updated script
    Changed the script to specify the hostname for the target directory and left out the password to be filled by user. This should be run as a cron job, and should be made executable without read or write perms.

--- shell-scripts/buscriptSQL.sh
+++ shell-scripts/buscriptSQL.sh
@@ -2,5 +2,6 @@
 TIME=`date +%F-%H%M%S`                   # time stamp to the backup file
 FILENAME="SQL-backup-$TIME.sql"      # defining the format of the file name while backing up
 SRCDIR="mysql"                     # Location of the backup directory
-DESDIR="/media/Fileshare/backups/sysfs/web1"                   #  Destination of the backup directory (where you need to backup)
-mysqldump --all-databases >  $DESDIR/$FILENAME -u root -p38UrSjMyJQAy4CHr
+FOLDNAME = `hostname `
+DESDIR="/media/Fileshare/backups/sysfs/$FOLDNAME"   #  Destination of the backup. Assumes machine name as folder name
+mysqldump --all-databases >  $DESDIR/$FILENAME -u root -p[password]
